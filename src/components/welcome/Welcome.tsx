d3d210bd33ac5bc2d5db8971385bf0aceba15a73
BauveR <rick.bauve@gmail.com>
2026-01-21 21:41:02 +0100

    automatic cloudinary optimization images

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -3,9 +3,9 @@ import { useState, useEffect } from "react";
 import leidenLogo from "../../assets/UniversiteitLeidenLogo.png";
 import ullLogo from "../../assets/logo-ull-nuevo-blanco.png";
 
-const desktopImage = "https://res.cloudinary.com/dmweipuof/image/upload/v1768775626/corpus_colonia_desktop_t90sru.png";
-const tabletImage = "https://res.cloudinary.com/dmweipuof/image/upload/v1768775628/pantallas-03_tlu7r2.png";
-const mobileImage = "https://res.cloudinary.com/dmweipuof/image/upload/v1768775625/pantallas-02_wdzoos.png";
+const desktopImage = "https://res.cloudinary.com/dmweipuof/image/upload/f_auto,q_auto,w_1920/v1768775626/corpus_colonia_desktop_t90sru.png";
+const tabletImage = "https://res.cloudinary.com/dmweipuof/image/upload/f_auto,q_auto,w_1536/v1768775628/pantallas-03_tlu7r2.png";
+const mobileImage = "https://res.cloudinary.com/dmweipuof/image/upload/f_auto,q_auto,w_828/v1768775625/pantallas-02_wdzoos.png";
 
 const getInitialImage = () => {
   if (typeof window === 'undefined') return desktopImage;
